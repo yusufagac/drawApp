5638214ee420f1d7ae7593f4e5bdb34414b403d5
Yusuf Ağaç <43214120+yusufagac@users.noreply.github.com>
2022-02-23 14:54:40 +0000

    Drawing application basics

--- main.cs
+++ main.cs
@@ -1,7 +1,11 @@
 using System;
+using System.Collections.Generic;
 
 class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
-}
+      static void Main(string[] args) {
+         var random = new Random();
+         var list = new List<string>{"one","two","three","four"};
+         int index = random.Next(list.Count);
+         Console.WriteLine(list[index]);
+      }
+   }
