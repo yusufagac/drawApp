9e2055ce10fec4923eb1c9f51f0200b0d847f951
Yusuf Ağaç <43214120+yusufagac@users.noreply.github.com>
2022-02-25 14:32:38 +0000

    Application has been released!

--- main.cs
+++ main.cs
@@ -1,11 +1,37 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 
 class Program {
-      static void Main(string[] args) {
-         var random = new Random();
-         var list = new List<string>{"one","two","three","four"};
-         int index = random.Next(list.Count);
-         Console.WriteLine(list[index]);
-      }
-   }
+    static void Main(string[] args) { 
+      
+      while(true){
+            Console.WriteLine("Aralarina virgul koyarak isimleri yazin.");
+            Console.Write("Giris: ");
+            string text = Console.ReadLine();
+            string[] split = text.Split(',');
+      
+                Random rnd = new Random();
+                int index = rnd.Next(split.Length);
+                Console.WriteLine($"Name: {split[index]}");                      
+                    while(true){
+                        Console.WriteLine("Devam edilsin mi? Y/N");
+                         string bitir = Console.ReadLine();
+                            if(bitir=="Y"){
+                                     break;                     
+                                }
+                                else if(bitir=="N"){
+                                      Console.WriteLine("Program sonlandiriliyor...");
+                                      break;
+                                }
+                                else {
+                                    
+                                     continue; 
+                                }
+                      }
+                break;
+                            
+        }
+            Console.WriteLine("Yine bekleriz...");
+    }
+}
