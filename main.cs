87d4491aa67cb98a567adf5b5d275213855b1d7f
Yusuf Ağaç <43214120+yusufagac@users.noreply.github.com>
2022-02-25 13:41:20 +0000

    Draw application is completed

--- main.cs
+++ main.cs
@@ -4,18 +4,40 @@ using System.IO;
 
 class Program {
     static void Main(string[] args) {   
-          string text;   
-          var fileStream = new FileStream(@"c:\oku.txt", FileMode.Open, FileAccess.Read);
-          using (var streamReader = new StreamReader(fileStream))
-          {
-              text = streamReader.ReadToEnd();
-          }
-    
+      
+      while(true){
+            Console.WriteLine("Aralarina virgul koyarak isimleri yazin.");
+            Console.Write("Giris: ");
+            string text = Console.ReadLine();
             string[] split = text.Split(',');
       
                 Random rnd = new Random();
                 int index = rnd.Next(split.Length);
-                Console.WriteLine($"Name: {split[index]}");                    
+                Console.WriteLine($"Name: {split[index]}");                      
+                    while(true){
+                        Console.WriteLine("Devam edilsin mi? Y/N");
+                         string bitir = Console.ReadLine();
+                            if(bitir=="Y"){
+                                     break;                     
+                                }
+                                else if(bitir=="N"){
+                                      Console.WriteLine("Program sonlandiriliyor...");
+                                      break;
+                                }
+                                else {
+                                     continue; 
+                                }
+                      }
+                            
+        }
+            Console.WriteLine("Yine bekleriz...");
     }
 }
+
+/*string text;   
+          var fileStream = new FileStream(@"c:\oku.txt", FileMode.Open, FileAccess.Read);
+          using (var streamReader = new StreamReader(fileStream))
+          {
+              text = streamReader.ReadToEnd();
+          }*/
 
